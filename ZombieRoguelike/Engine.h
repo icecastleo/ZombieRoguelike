81f41a583275bbd762878f70ca22b34df8b8de94
FrozenIce <icecastle221@gmail.com>
2016-10-02 18:10:32 -0400

    Tutorial part 3: dungeon building

---
+++ ZombieRoguelike/Engine.h
@@ -0,0 +1,14 @@
+#pragma once
+class Engine {
+public:
+	TCODList<Actor *> actors;
+	Actor *player;
+	Map *map;
+
+	Engine();
+	~Engine();
+	void update();
+	void render();
+};
+
+extern Engine engine;
